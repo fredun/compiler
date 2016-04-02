eaa1d787c5972060fb280053da451dc696194bb2
Sam Rijs <srijs@airpost.net>
2016-04-03 02:39:57 +1000

    Core.TypeLevel: kind of constant

--- src/Core/TypeLevel.hs
+++ src/Core/TypeLevel.hs
@@ -23,3 +23,10 @@ data Type =
 data Binding =
     TypeVariableBinding Text Kind
   | TermVariableBinding Text Type
+
+kindOfConstant :: Constant -> Kind
+kindOfConstant PrimitiveConstant = KindOfTypes
+kindOfConstant FunctionConstant = KindOfTypeConstructors KindOfTypes (KindOfTypeConstructors KindOfTypes KindOfTypes)
+kindOfConstant (ForAllConstant k) = KindOfTypeConstructors (KindOfTypeConstructors k KindOfTypes) KindOfTypes
+kindOfConstant (ExistsConstant k) = KindOfTypeConstructors (KindOfTypeConstructors k KindOfTypes) KindOfTypes
+kindOfConstant (RecordConstant _) = KindOfTypes
