52ade39434fe5034d44fad0aa70cbd6745124c8c
Sam Rijs <srijs@airpost.net>
2016-04-03 02:03:28 +1100

    fix kind structures in type-level core

--- src/Core/TypeLevel.hs
+++ src/Core/TypeLevel.hs
@@ -6,14 +6,19 @@ import Data.Text (Text)
 data Constant =
     PrimitiveConstant
   | FunctionConstant Constant Constant
+  | ForAllConstant Kind
   | RecordConstant (Set Text)
 
 data Kind =
     KindOfTypes
-  | KindOfTypeConstructors
+  | KindOfTypeConstructors Kind Kind
 
 data Type =
     Constant Constant
   | Variable Text
   | Abstraction Text Kind Type
   | Application Type Type
+
+data Binding =
+    TypeVariableBinding Text Kind
+  | TermVariableBinding Text Type
