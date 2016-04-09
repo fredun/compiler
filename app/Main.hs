a9ede2e63a022d4a42575082e2596cc2d2f8efb1
Sam Rijs <srijs@airpost.net>
2016-04-09 18:04:07 +1000

    Main: try to parse from stdin

--- app/Main.hs
+++ app/Main.hs
@@ -1,3 +1,18 @@
 module Main where
 
-main = return ()
+import qualified Text.PrettyPrint.ANSI.Leijen as Doc
+import qualified Text.Trifecta as Trifecta
+
+import qualified Data.ByteString as ByteString
+
+import qualified IR.Parser as Parser
+
+main :: IO ()
+main = do
+  contents <- ByteString.getContents
+  let result = Trifecta.parseByteString Parser.term mempty contents
+  case result of
+    Trifecta.Failure err ->
+      Doc.putDoc (mappend err Doc.linebreak)
+    Trifecta.Success term ->
+      print term
